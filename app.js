e529676cf60903795e6c126eb5d7c5f83e82edc8
SergeyChaplinskiy <serz.4a5linskiy.gmail.com>
2022-06-07 01:57:42 +0300

    Add all my files

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 
 const os = require('os');
-const infoIp = require('./getIp');
 
-const networkInterfaces = os.networkInterfaces();
+const infoIp = () => {
+    const paramIp = os.networkInterfaces();
+    const ip_ = [];
+    let scor = 1;
 
-const op = infoIp(networkInterfaces);
-console.log(op);
+    Object.keys(paramIp).forEach(function(name){
+        paramIp[name].forEach(function (iface) {
+            if(iface.family === 4 && iface.internal !== false){
+                localIp = iface.address;
+                ip_.push(`local ip address: ${localIp}`);
+            }
+            else if(iface.family === 4 && iface.internal === false){
+                externalIp = iface.address;
+                ip_.push(`external ip address(v4) ${+scor}: ${externalIp} device name "${name}"`);
+                scor++;
+            }else if(iface.address === undefined){
+                ip_.push(`ip address not found: 0.0.0.0`);
+            }
+        })
+    })
+    return ip_;
+}
+
+module.exports = infoIp();
+
+console.log(infoIp());
